8c97bebb509f2def86fbd6092e539b8f2916863e
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-02-18 16:23:15 +0100

    create page MAtchPage

---
+++ src/components/MatchPage.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+import './MatchPage.css'
+export default function MatchPage() {
+    return (
+        <div className="MatchPage_container">
+            
+        </div>
+    )
+}
