14dfc5d43948ce2eb0394c6c00c05734e45fda00
oliviermod8 <oliviermod8@gmail.com>
2021-02-19 00:25:16 +0100

    modif

--- src/components/Jeucopy.js
+++ src/components/Jeucopy.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+// import { Link } from "react-router-dom";
 import axios from 'axios'
 
 import Card from './Card'
@@ -6,47 +7,60 @@ import Card from './Card'
 import './Jeucopy.css'
 import './BoutonMatch.css'
 
+export default function Jeucopy () {
+    const [StarsData, setStarsData] = useState ([""]);
+    const [ImgRandom, setImgRandom] = useState (['']);
 
-export default class Jeucopy extends Component {
-    state = {
-        StarsData : [],
+    useEffect(() => {
+
+    async function fetchData(){
+        const req = await axios.get("https://miadil.github.io/starwars-api/api/all.json")
+        setStarsData(req.data)
+        StarsData.map(star => (
+            setImgRandom(star.image)
+        ))
     }
+    fetchData()
+   
+    },[])
 
-    componentDidMount() {
-        axios
-                // .get('https://miadil.github.io/starwars-api/api/all.json')
-                .get('https://simpsons-quotes-api.herokuapp.com/quotes')
-                .then((res) => this.setState({StarsData : res.data}))
-    }
-
-    render() {
-        const {StarsData}  = this.state
-        // const RandomStars = StarsData.map[Math.floor(Math.random() * StarsData.map.lenght)]
-
-        // document.body.innerHTML = RandomStars
-        
+     const random =Math.floor(Math.random() * StarsData.length)
+    setImgRandom(StarsData[random].image)
+    // console.log(ImgRandom)
         return (
             <div className="Jeu">
                 <h1 className="TitreJeu">La roue de l'amour 💘 ?</h1>
 
                 <div id="tableauJeu">
                     <div className="Card">
-                       {StarsData.map((item) => (
-                            <Card {...item} key={item.id}/>
-                        ))}
+                     
                     </div> 
                     <p id="icon18">🔞</p>  
                     <div className="Card">
-                       {StarsData.map((item) => (
-                            <Card {...item} key={item.id}/>
-                        ))}  
+                        {/* {StarsData.map(starData => (
+                            <img src={starData.image} alt="image"></img>
+                        ))} */}
                     </div>       
                 </div> 
-                <button id="buttonMatch">
+                {/* pb pour lier action au boutton */}
+                <button type="button" id="buttonMatch" >
                     Etes vous faits l'un pour l'autre ? 
                      <span id="hart">🌈💖🔥</span>
                 </button>
+
+                {/* <Link
+                    to={{
+                     pathname: "/Caro_Kev",
+                     data: {
+                      Datasurlespersos: this.props.location.data.Datasurlespersos,
+                    },
+                    }}
+                 className="xxxxxxxxxxx"
+                >
+                 Let's go Fight
+                </Link> */}
+
             </div>
         )
-    }
 }
+
