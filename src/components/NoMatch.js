5022e00249df58a137c75f9d8dd29a0d99445a34
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-02-18 23:40:06 +0100

    game r ready

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,76 +1,10 @@
-// import React, { useState, useEffect } from 'react';
-// import arms from './Arms';
-// import './NoMatch.css';
-
-// export default function NoMatch() {
-//   const [armsImg, setArmsImg] = useState('');
-//   const [launch, setLaunch] = useState('Start');
-//   const [armsImgAi, setArmsImgAI] = useState('');
-//   const [stop, setStop] = useState('');
-//   const [BtnStart, setBtnStart] = useState('invisible');
-
-//   //Choose play weapon
-//   const handleChange = (e) => {
-//     setArmsImg(e.target.value);
-//     setBtnStart('');
-//   };
-
-//   Math.floor(Math.random() * 10 + 1);
-//   // Start Battle and put weapon IA
-//   const startGame = () => {
-//     setLaunch('No love me !!!!!!');
-//     setArmsImgAI(arms[Math.floor(Math.random() * arms.length)].img);
-//     setStop('invisible');
-//   };
-
-//   return (
-//     <div className="noMatch_container">
-//       <div className="noMatch_armes">
-//         <div className="noMAtch_user_weapon">
-//           <p>Choisissez votre arme brise coeur</p>
-//           <select className={stop} onChange={handleChange}>
-//             {arms.map((arm) => (
-//               <option value={arm.img}>{arm.Arme}</option>
-//             ))}
-//           </select>
-//           <div className="noMatch_user_select_arm">
-//             <img className="img-weapon" src={armsImg} alt={armsImg}></img>
-//           </div>
-//         </div>
-//         <div className="noMAtch_ai_weapon">
-//           <p>L'arme de votre adversaire</p>
-//           <br></br>
-//           <img className="img-weapon" src={armsImgAi}></img>
-//         </div>
-//       </div>
-//       <div className="noMatch_candidat_container">
-//         <img
-//           className="noMatch_img_candidat"
-//           src="https://images.unsplash.com/photo-1559717642-f78162471caf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-//           alt="name"
-//         />
-//         {/*bloc candidat 1*/}
-//         <button className={BtnStart} onClick={startGame}>
-//           {launch}
-//         </button>
-//         <img
-//           className="noMatch_img_candidat"
-//           src="https://images.unsplash.com/photo-1546314029-ca8247095938?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
-//           alt="name"
-//         />
-//         {/*bloc candidat 2*/}
-//       </div>
-//       <button>Peut être une dernière chance de passer une bonne soirée</button>
-//     </div>
-//   );
-// }
-
 import React, { useState, useEffect } from 'react';
 import arms from './Arms';
 import './NoMatch.css';
 
 export default function NoMatch() {
   const [armsImg, setArmsImg] = useState('');
+  const [UserArmsScore, setUserArmsScore] =useState('')
   const [launch, setLaunch] = useState('Start');
   const [armsImgAi, setArmsImgAI] = useState('');
   const [stop, setStop] = useState('');
@@ -81,27 +15,45 @@ export default function NoMatch() {
   const [imgVisibleAi, setImgVisibleAi] = useState('invisible')
   const [messageCombat, setMessageCombat] = useState('invisible')
   const [messageNewChance, setMessageNewChance] = useState("invisible")
+  const [red, setRed] = useState('red')
+  const [SuperFight, setSuperFight] = useState('Combat en cour ...')
 
   //Choose play weapon
+
   const handleChange = (e) => {
-    setArmsImg(e.target.value);
-    setBtnStart('');
+    const mychoice = e.target.value
+    setUserArmsScore(e.target[e.target.selectedIndex].id)
+    setArmsImg(mychoice)
+    setBtnStart('')
     setUserMessage('Voici votre arme')
     setImgVisible('')
   };
 
-  Math.floor(Math.random() * 10 + 1);
+
+
   // Start Battle and put weapon IA
   const startGame = () => {
     const random =Math.floor(Math.random() * arms.length)
     setImgVisibleAi('')
     setArmsImgAI(arms[random].img);
+    const scoreArmAi = arms[random].Puissance
     setStop('invisible');
     setBtnStart('invisible');
     setAiMessage(`Voici l'arme de votre adversaire " ${arms[random].Arme} "`)
     setMessageCombat('')
     setMessageNewChance('')
+    setTimeout(
+      function(){ 
+        if(scoreArmAi < UserArmsScore){
+          setSuperFight(" La win Ingrid et Caro && Jacquie && Michel ")
+        }else if(scoreArmAi === UserArmsScore){
+          setSuperFight(" Egalite ")
+        }else{
+          setSuperFight("T'as perdu ")
+        }
+    }, 3000);
   };
+    
 
   return (
     <div className="noMatch_container">
@@ -110,7 +62,7 @@ export default function NoMatch() {
           <p> {UserMessage} </p>
           <select className={stop} onChange={handleChange}>
             {arms.map((arm) => (
-              <option value={arm.img}>{arm.Arme}</option>
+              <option value={arm.img} className="choiceweapon" id={arm.Puissance} >{arm.Arme}</option>
             ))}
           </select>
           <div className="noMatch_user_select_arm">
@@ -127,7 +79,7 @@ export default function NoMatch() {
         <button className={`btnStartFight ${BtnStart}`} onClick={startGame}>
           {launch}
         </button>
-        <span className={`spanCombatEnCour ${messageCombat}`}>Combat en Cour...</span>
+        <span className={`spanCombatEnCour ${messageCombat}`} style={{color:`${red}`}}>{SuperFight}</span>
         <div className="noMAtch_ai_weapon">
             <p>{AiMessage}</p>
             <img className={`img-weapon ${imgVisibleAi}`} alt='img' src={armsImgAi}></img>
