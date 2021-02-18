146438930d14c4d0013f0f51fd3ba59a76cf97b0
oliviermod8 <oliviermod8@gmail.com>
2021-02-18 17:02:42 +0100

    add api pics

--- src/components/Jeu.js
+++ src/components/Jeu.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 import './Jeu.css'
 
-
 export default function Jeu ()  {
-        return (
+    const [StarsData, setStarsData] = useState("");
+
+    const url = 'https://miadil.github.io/starwars-api/api/all.json';
+
+    useEffect(() => {
+       async function request () {
+        const res = await axios.get(url)
+        setStarsData(res.data)
+       }
+       request()
+    },[])
+// console.log(StarsData)
+    return (
             <div className="Jeu">
                 <h1 className="TitreJeu">La roue de l'amour 💘 ?</h1>
 
                 <div id="tableauJeu">
                     <div className="Card">
-                    <img className="ImageJeu" src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg" alt="Luke"/>
-                    <div id="ligneJeu">Name Player</div>  
+                    <img className="ImageJeu" src={setStarsData[0].image} alt="Luke"/>
+                    <div id="ligneJeu">{setStarsData[0].name}</div>  
                     </div> 
                     <p id="icon18">🔞</p>  
                     <div className="Card">
@@ -21,5 +33,4 @@ export default function Jeu ()  {
                 </div> 
             </div>
         )
-    }
-
+}    
