51bb42b68e98b7713149c7862106687389b25573
Alexandre-Rosier <alex.rosier1405@gmail.com>
2021-02-19 12:01:07 +0100

    css page match

--- src/components/Match.js
+++ src/components/Match.js
@@ -12,14 +12,9 @@ export default class Match extends Component {
 
       <div className="Match_coeurs_container">
           <img
-            className="Match_img_coeurs"
-            src="images/matchCoeur1.png"
-            alt="coeur"
-          />
-          <img
-            className="Match_img_coeurs"
-            src="images/matchCoeur2.png"
-            alt="coeur"
+            className="Match_img_loveroom"
+            src="imageAlex/loveroom.png"
+            alt="loveroom"
           />
       </div>
 
@@ -29,7 +24,7 @@ export default class Match extends Component {
             src="https://images.unsplash.com/photo-1559717642-f78162471caf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
             alt="name"
           />
-            <button className="btnStartFight" onClick={this.test}>ça t'excite ???</button>
+            <button className="btnStartFight" onClick={this.test}>Est-ce que tu veux pusher avec moi ???</button>
           <img
             className="Match_img_candidat"
             src="https://images.unsplash.com/photo-1546314029-ca8247095938?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80"
